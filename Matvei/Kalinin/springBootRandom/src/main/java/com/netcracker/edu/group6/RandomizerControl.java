bee931c4a6354fc1c8b41862a2742bfa737edc02
Kalinin-M-S <Kalinin_m_s@mail.ru>
2019-12-27 14:12:24 +0400

    Revert "Add files via upload"
    This reverts commit d0ac401d0c3ec2b82daaec1399b4eb4f996770d0.

--- Matvei/Kalinin/springBootRandom/src/main/java/com/netcracker/edu/group6/RandomizerControl.java
+++ Matvei/Kalinin/springBootRandom/src/main/java/com/netcracker/edu/group6/RandomizerControl.java
@@ -1,7 +1,5 @@
 package com.netcracker.edu.group6;
 
-import java.util.Random;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +8,12 @@ import com.netcracker.edu.group6.Randomizer;
 
 @RestController
 public class RandomizerControl {
+
+	int result = Randomizer.getNumber();
 	
 	@RequestMapping("/test2/test2")
 	@ResponseBody
 	String rand() {
-		
-		Random rnd = new Random(System.currentTimeMillis());
-		int number = rnd.nextInt(1000);
-		return "Your number is " + number;
+		return "Your number is " + result;
 	}
 }
