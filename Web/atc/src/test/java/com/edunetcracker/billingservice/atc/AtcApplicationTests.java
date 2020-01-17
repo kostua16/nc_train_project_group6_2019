7bc4c27bf5aeb8bcf9bc01f5a119f250967ed360
timurka169 <timurka335@gmail.com>
2020-01-17 17:16:51 +0300

    push spring1

---
+++ Web/atc/src/test/java/com/edunetcracker/billingservice/atc/AtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edunetcracker.billingservice.atc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
