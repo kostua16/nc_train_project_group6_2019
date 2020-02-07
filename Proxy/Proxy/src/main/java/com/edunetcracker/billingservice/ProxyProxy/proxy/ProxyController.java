f9c9e06e1f1c39d2ac530639de78a3dba4766e13
ZZZDanil <ru.yandex.zzdanil>
2020-02-07 14:11:48 +0400

    Added Test Validation and Test Rabbit

--- Proxy/Proxy/src/main/java/com/edunetcracker/billingservice/ProxyProxy/proxy/ProxyController.java
+++ Proxy/Proxy/src/main/java/com/edunetcracker/billingservice/ProxyProxy/proxy/ProxyController.java
@@ -1,14 +1,18 @@
 package com.edunetcracker.billingservice.ProxyProxy.proxy;
 
-import org.springframework.http.*;
+import com.edunetcracker.billingservice.ProxyProxy.rabbit.RabbitMQSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ProxyController {
+
+    @Autowired
+    RabbitMQSender rabbitMQSender;
 
     @GetMapping("/proxy/test/")
     public String getTest(@RequestParam("str") String str) {
@@ -23,12 +27,28 @@ public class ProxyController {
         //ResponseEntity<Integer> responseDataBase = restTemplate.exchange(urlDataBase, HttpMethod.GET, new HttpEntity(new HttpHeaders()), Integer.class);
 
         //int result = responseDataBase.getBody();
+
+        //rabbitMQSender.send("This is test message!");
+
         long result =  new Rando().getRandInt(-2L,3L);
         if(result<0){
             return new ResponseEntity<Long>((Long)null, HttpStatus.NOT_FOUND); //NOT_FOUND //BAD_REQUEST //INTERNAL_SERVER_ERROR
             //return null;
         }
         else
+            return new ResponseEntity<Long>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/proxy/getBalanceWithRabbitMessage/")
+    public ResponseEntity<Long> getBalanceWithRabbitMessage(@RequestParam("id") Long id, @RequestParam("message") Boolean optionMessage) {
+
+        long result =  new Rando().getRandInt(-2L,3L);
+        if(optionMessage.equals(true)){
+            rabbitMQSender.send("This is test message! Rand = " + result);
+        }
+        if(result<0){
+            return new ResponseEntity<Long>((Long)null, HttpStatus.NOT_FOUND); //NOT_FOUND //BAD_REQUEST //INTERNAL_SERVER_ERROR
+        }else
             return new ResponseEntity<Long>(result, HttpStatus.OK);
     }
 
--- Proxy/Validator/src/main/java/com/edunetcracker/billingservice/ProxyValidator/validator/Logic.java
+++ Proxy/Validator/src/main/java/com/edunetcracker/billingservice/ProxyValidator/validator/Logic.java
@@ -1,19 +1,26 @@
 package com.edunetcracker.billingservice.ProxyValidator.validator;
 
 
+import com.edunetcracker.billingservice.ProxyValidator.entity.Account;
 import org.springframework.http.*;
 import org.springframework.lang.NonNull;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Map;
 
 
 @RestController
+@Validated
 public class Logic {
 
     @GetMapping("/validator/test/")
@@ -37,5 +44,27 @@ public class Logic {
         }
             //return responsePoxy;
             return new ResponseEntity<>(responsePoxy.getBody(), responsePoxy.getStatusCode());
+    }
+    @GetMapping("/validator/getBalanceWithValidation/")
+    public ResponseEntity<Long> getBalanceWithValidation(@RequestParam ("id") @Min(1) @Max(10) @NotNull Long id,
+                                                         @RequestParam("message") Boolean optionMessage) {
+    //обращение к proxy
+    final String urlDataBase = "http://localhost:8102/proxy/getBalanceWithRabbitMessage/"+"?id="+ + id + "&message="+optionMessage;
+    RestTemplate restTemplate = new RestTemplate();
+    ResponseEntity<Long> responsePoxy = null;
+        try {
+            responsePoxy = restTemplate.exchange(urlDataBase, HttpMethod.GET, new HttpEntity(new HttpHeaders()), Long.class);
+        } catch (HttpClientErrorException.NotFound e) {
+            return new ResponseEntity<>((Long) null, HttpStatus.NOT_FOUND);
+        } catch (HttpClientErrorException e) {
+            return new ResponseEntity<>((Long) null, HttpStatus.NOT_FOUND);  //BAD_REQUEST
+        }
+        return new ResponseEntity<>(responsePoxy.getBody(), responsePoxy.getStatusCode());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
+        return new ResponseEntity<>("not valid due to validation error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
