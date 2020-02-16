d443f00497005eb5decbc1be42c9348f13a328ab
ZZZDanil <ru.yandex.zzdanil>
2020-02-16 15:54:26 +0400

    Proxy validator and proxy corrections

--- Proxy/Proxy/src/main/java/com/edunetcracker/billingservice/ProxyProxy/proxy/ProxyController.java
+++ Proxy/Proxy/src/main/java/com/edunetcracker/billingservice/ProxyProxy/proxy/ProxyController.java
@@ -2,6 +2,7 @@ package com.edunetcracker.billingservice.ProxyProxy.proxy;
 
 import com.edunetcracker.billingservice.ProxyProxy.rabbit.RabbitMQSender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,19 +22,10 @@ public class ProxyController {
 
     @GetMapping("/proxy/getBalance/")
     public ResponseEntity<Long> getBalance(@RequestParam("id") Long id) {
-        //обращение к базе данных
-        //final String urlDataBase = "http://localhost:8080/DB/getBalance/" + id;
-        //RestTemplate restTemplate = new RestTemplate();
-        //ResponseEntity<Integer> responseDataBase = restTemplate.exchange(urlDataBase, HttpMethod.GET, new HttpEntity(new HttpHeaders()), Integer.class);
-
-        //int result = responseDataBase.getBody();
-
-        //rabbitMQSender.send("This is test message!");
 
         long result =  new Rando().getRandInt(-2L,3L);
         if(result<0){
             return new ResponseEntity<Long>((Long)null, HttpStatus.NOT_FOUND); //NOT_FOUND //BAD_REQUEST //INTERNAL_SERVER_ERROR
-            //return null;
         }
         else
             return new ResponseEntity<Long>(result, HttpStatus.OK);
--- Proxy/Validator/src/main/java/com/edunetcracker/billingservice/ProxyValidator/validator/ValidatorController.java
+++ Proxy/Validator/src/main/java/com/edunetcracker/billingservice/ProxyValidator/validator/ValidatorController.java
@@ -2,6 +2,7 @@ package com.edunetcracker.billingservice.ProxyValidator.validator;
 
 
 import com.edunetcracker.billingservice.ProxyValidator.entity.Account;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.lang.NonNull;
 import org.springframework.validation.BindingResult;
@@ -21,7 +22,30 @@ import java.util.Map;
 
 @RestController
 @Validated
-public class Logic {
+public class ValidatorController {
+
+    @Value("${app.proxy_proxy.host}")
+    String hostProxy = "localhost";
+
+    @Value("${app.proxy_proxy.port}")
+    String portProxy = "8102";
+
+    private String getUrlProxy (){
+        return "http://"+ hostProxy + ":" + portProxy;
+    }
+
+    private ResponseEntity returnResponseFromUrl (String url){
+        ResponseEntity<Long> responsePoxy = null;
+        try {
+            responsePoxy = new RestTemplate().exchange(url, HttpMethod.GET, new HttpEntity(new HttpHeaders()), Long.class);
+        } catch (HttpClientErrorException.NotFound e){
+            return new ResponseEntity<>((Long)null, HttpStatus.NOT_FOUND);
+        }
+        catch (HttpClientErrorException e){
+            return new ResponseEntity<>((Long)null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(responsePoxy.getBody(), responsePoxy.getStatusCode());
+    }
 
     @GetMapping("/validator/test/")
     public String getTest(@RequestParam("str") String str) {
@@ -31,38 +55,18 @@ public class Logic {
     @GetMapping("/validator/getBalance/")
     public ResponseEntity<Long> getBalance(@RequestParam ("id") Long id) {
         //обращение к proxy
-        final String urlDataBase = "http://localhost:8102/proxy/getBalance/"+"?id="+ + id;
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Long> responsePoxy = null;
-        try {
-            responsePoxy = restTemplate.exchange(urlDataBase, HttpMethod.GET, new HttpEntity(new HttpHeaders()), Long.class);
-        } catch (HttpClientErrorException.NotFound e){
-            return new ResponseEntity<>((Long)null, HttpStatus.NOT_FOUND);
-        }
-        catch (HttpClientErrorException e){
-            return new ResponseEntity<>((Long)null, HttpStatus.NOT_FOUND);
-        }
-            //return responsePoxy;
-            return new ResponseEntity<>(responsePoxy.getBody(), responsePoxy.getStatusCode());
+        final String urlDataBase = getUrlProxy() + "/proxy/getBalance/"+"?id="+ + id;
+        return returnResponseFromUrl(urlDataBase);
     }
     @GetMapping("/validator/getBalanceWithValidation/")
     public ResponseEntity<Long> getBalanceWithValidation(@RequestParam ("id") @Min(1) @Max(10) @NotNull Long id,
                                                          @RequestParam("message") Boolean optionMessage) {
-    //обращение к proxy
-    final String urlDataBase = "http://localhost:8102/proxy/getBalanceWithRabbitMessage/"+"?id="+ + id + "&message="+optionMessage;
-    RestTemplate restTemplate = new RestTemplate();
-    ResponseEntity<Long> responsePoxy = null;
-        try {
-            responsePoxy = restTemplate.exchange(urlDataBase, HttpMethod.GET, new HttpEntity(new HttpHeaders()), Long.class);
-        } catch (HttpClientErrorException.NotFound e) {
-            return new ResponseEntity<>((Long) null, HttpStatus.NOT_FOUND);
-        } catch (HttpClientErrorException e) {
-            return new ResponseEntity<>((Long) null, HttpStatus.NOT_FOUND);  //BAD_REQUEST
-        }
-        return new ResponseEntity<>(responsePoxy.getBody(), responsePoxy.getStatusCode());
+        //обращение к proxy
+        final String urlDataBase = getUrlProxy() + "/proxy/getBalanceWithRabbitMessage/"+"?id="+ + id + "&message="+optionMessage;
+        return returnResponseFromUrl(urlDataBase);
     }
 
-    @ExceptionHandler(ConstraintViolationException.class)
+    @ExceptionHandler(ConstraintViolationException.class) //@ExceptionHandler({1.class, 2.class, 3.class}) //для нескольких исключений
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
         return new ResponseEntity<>("not valid due to validation error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
