a0f08286f2350d851c5f50297de93996f0d73d95
Kalinin-M-S <Kalinin_m_s@mail.ru>
2020-02-19 15:02:16 +0400

    In-progress

--- Billing/Billing-DB/src/main/java/com/edunetcracker/billingservice/BillingDB/services/BillingDBController.java
+++ Billing/Billing-DB/src/main/java/com/edunetcracker/billingservice/BillingDB/services/BillingDBController.java
@@ -1,11 +1,25 @@
 package com.edunetcracker.billingservice.BillingDB.services;
 
+import com.edunetcracker.billingservice.BillingDB.entity.Account;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 //TODO
 
@@ -19,6 +33,19 @@ public class BillingDBController {
 
     Logger logger = LoggerFactory.getLogger(BillingDBController.class);
 
+    @GetMapping(path = "byNumber/{NUMBER}", produces = APPLICATION_JSON_VALUE)
+    @Async
+    public CompletableFuture<ResponseEntity<Account>> getOneAccountByNumber(@PathVariable("NUMBER") @Valid @Min(100000) @NotNull Long number){
+        final Optional<Account> byNumber = repository.findByNumber(number);
 
+        if(logger.isDebugEnabled()){
+            logger.debug("getOneAccountByNumber[number = {}, result = {}]", number, byNumber);
+        }
 
+        if(byNumber.isPresent()){
+            return CompletableFuture.completedFuture(new ResponseEntity<Account>(byNumber.get(), HttpStatus.OK));
+        } else {
+            return CompletableFuture.completedFuture(new ResponseEntity<Account>((Account) null, HttpStatus.NOT_FOUND));
+        }
+    }
 }
--- Billing/Billing-DB/src/main/java/com/edunetcracker/billingservice/BillingDB/services/IAccountRepository.java
+++ Billing/Billing-DB/src/main/java/com/edunetcracker/billingservice/BillingDB/services/IAccountRepository.java
@@ -16,21 +16,12 @@ import java.util.List;
 @Service
 public interface IAccountRepository {
 
-    @Query("select b from #{#entityName} b where b.isbn = upper(:isbn_value)")
-    List<Account> customFindByMSISDN(@Param("isbn_value")  String isbn);
-
     Page<Account> findByNameIsNotNull(Pageable pageable);
-    Page<Account> findByISBNIsNotNull(Pageable pageable);
     Page<Account> findByNameContaining(String name, Pageable pageable);
     Page<Account> findByNameNotContaining(String name, Pageable pageable);
-    Page<Account> findByIsbnContaining(String isbn, Pageable pageable);
-    Page<Account> findByIsbnNotContaining(String isbn, Pageable pageable);
 
     Page<Account> findByName(String name, Pageable pageable);
-    Page<Account> findByIsbn(String isbn, Pageable pageable);
-
-    @Procedure("setBookName")
-    void setName(BigInteger id, String name);
+    Page<Account> findByNumber(Long number, Pageable pageable);
 
     @Query("select count(b) from #{#entityName} b where b.id is not null")
     Integer countAll();
