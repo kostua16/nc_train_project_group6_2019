aa4d5e4a8bb1072f01d875cc5deb2426726afecc
Kalinin-M-S <Kalinin_m_s@mail.ru>
2020-03-11 15:02:53 +0400

    More DB updates

--- Billing/Billing-DB/src/main/java/com/edunetcracker/billingservice/BillingDB/services/BillingDBController.java
+++ Billing/Billing-DB/src/main/java/com/edunetcracker/billingservice/BillingDB/services/BillingDBController.java
@@ -1,18 +1,19 @@
 package com.edunetcracker.billingservice.BillingDB.services;
 
 import com.edunetcracker.billingservice.BillingDB.entity.Account;
+import com.edunetcracker.billingservice.BillingDB.entity.AccountSearchResult;
+import com.edunetcracker.billingservice.BillingDB.entity.SearchRequest;
+import com.edunetcracker.billingservice.BillingDB.entity.SearchResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
@@ -48,5 +49,73 @@ public class BillingDBController {
         } else {
             return CompletableFuture.completedFuture(new ResponseEntity<Account>((Account) null, HttpStatus.NOT_FOUND));
         }
+    }
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    @Async
+    public CompletableFuture<ResponseEntity<AccountSearchResult>> searchBooks(@Valid SearchRequest request ){
+        AccountSearchResult result = new AccountSearchResult();
+
+
+        String searchType = request.getType("id");
+        String searchCriteria = request.getCriteria("contains");
+        String searchQuery = request.getQuery();
+        Pageable searchParameters = request.getPageParameters(searchType);
+        result.setPageDetail(searchParameters);
+
+        /*
+        if("name".equals(searchType)){
+            if(request.isQueryEmpty()){
+                result.from(repository.findByNameIsNotNull(searchParameters));
+            } else {
+                if ("contains".equals(searchCriteria)) {
+                    result.from(repository.findByNameContaining(searchQuery, searchParameters));
+                } else if ("equals".equals(searchCriteria)) {
+                    result.from(repository.findByName(searchQuery, searchParameters));
+                } else if ("not_contains".equals(searchCriteria)) {
+                    result.from(repository.findByNameNotContaining(searchQuery, searchParameters));
+                }
+            }
+
+        }
+
+        if("number".equals(searchType)){
+            if(request.isQueryEmpty()){
+                result.from(repository.findByNumberIsNotNull(searchParameters));
+            } else {
+                if("contains".equals(searchCriteria)){
+                    result.from(repository.findByNumberContaining(searchQuery, searchParameters));
+                } else if ("equals".equals(searchCriteria)){
+                    result.from(repository.findByNumber(searchQuery, searchParameters));
+                }
+                else if ("not_contains".equals(searchCriteria)){
+                    result.from(repository.findByIsbnNotContaining(searchQuery, searchParameters));
+                }
+            }
+
+        }
+
+         */
+
+        if("number".equals(searchType)){
+            if(request.isQueryEmpty()){
+                result.from(repository.findAll(searchParameters));
+            } else {
+                Optional<Account> foundAccount;
+                try {
+                    foundAccount = repository.findByNumber(Long.parseLong(searchQuery));
+                } catch (NumberFormatException e){
+                    foundAccount = Optional.empty();
+                }
+                result.from(foundAccount);
+            }
+        }
+
+        if(logger.isDebugEnabled()){
+            logger.debug("searchBooks[request = {}, result = {}]", request, result);
+        }
+
+        return CompletableFuture.completedFuture(SearchResult.responseEntity(result));
     }
 }
