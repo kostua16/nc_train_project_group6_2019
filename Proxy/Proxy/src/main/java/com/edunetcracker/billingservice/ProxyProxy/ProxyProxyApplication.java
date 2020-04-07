f334abb772c91f92be2dc0741c14e9d633daaf72
timurka169 <timurka335@gmail.com>
2020-04-07 18:15:29 +0300

    ProxyProxyApplication add @Bean (corsFilter()) для конекта атс

--- Proxy/Proxy/src/main/java/com/edunetcracker/billingservice/ProxyProxy/ProxyProxyApplication.java
+++ Proxy/Proxy/src/main/java/com/edunetcracker/billingservice/ProxyProxy/ProxyProxyApplication.java
@@ -2,10 +2,28 @@ package com.edunetcracker.billingservice.ProxyProxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class ProxyProxyApplication {
-
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
     public static void main(String[] args) {
         SpringApplication.run(ProxyProxyApplication.class, args);
     }
