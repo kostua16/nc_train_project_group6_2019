8b0367fd957320b41bb1181dbfbbadeb44e9ffee
ZZZDanil <ru.yandex.zzdanil>
2020-04-23 15:31:46 +0400

    Prepare to heroku part 2

---
+++ Billing/Billing-DB/src/main/java/com/edunetcracker/billingservice/BillingDB/AccountController.java
@@ -0,0 +1,18 @@
+package com.edunetcracker.billingservice.BillingDB;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class AccountController {
+    @GetMapping("test")
+    public String t() {
+        return "Good";
+    }
+
+
+}
