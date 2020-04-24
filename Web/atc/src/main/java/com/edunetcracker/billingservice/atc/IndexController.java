6a48d52b9224ff254721295c6d761d7a30c241dd
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2020-04-24 19:43:26 +0400

    update properties for heroku deployment

--- Web/atc/src/main/java/com/edunetcracker/billingservice/atc/IndexController.java
+++ Web/atc/src/main/java/com/edunetcracker/billingservice/atc/IndexController.java
@@ -1,5 +1,6 @@
 package com.edunetcracker.billingservice.atc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class IndexController {
+
+    @Value("${app.validator.host}")
+    private String validatorHost;
+
+    @Value("${app.validator.port}")
+    private String validatorPort;
+
+    @GetMapping("validatorAddress")
+    public String getValidatorAddress(){
+        return "http://" + this.validatorHost + ":" + this.validatorPort;
+    }
 
 //    @GetMapping
 //    public String index(){
