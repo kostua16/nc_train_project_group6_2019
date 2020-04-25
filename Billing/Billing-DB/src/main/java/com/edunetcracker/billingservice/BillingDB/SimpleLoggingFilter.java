00b2b783a12b941705257f16d17ea3fdf2964113
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2020-04-25 21:49:09 +0400

    update all modules for heroku

---
+++ Billing/Billing-DB/src/main/java/com/edunetcracker/billingservice/BillingDB/SimpleLoggingFilter.java
@@ -0,0 +1,34 @@
+package com.edunetcracker.billingservice.BillingDB;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class SimpleLoggingFilter implements Filter {
+
+    Logger LOG = LoggerFactory.getLogger(SimpleLoggingFilter.class);
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        LOG.info("Starting a operation for request: {}, headers: {}" ,request.getRequestURI(), request.getHeaderNames());
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+        LOG.info("Finishing a operation for request: {}, headers: {}, responceStatus: {}" ,request.getRequestURI(), request.getHeaderNames(), response.getStatus());
+    }
+
+    public void init(FilterConfig filterConfig) {}
+
+    public void destroy() {}
+
+}
---
+++ Billing/Billing-DB/src/main/java/com/edunetcracker/billingservice/BillingDB/WebConfig.java
@@ -0,0 +1,31 @@
+package com.edunetcracker.billingservice.BillingDB;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${app.billing.url}")
+    private String billingUrl = "http://localhost:8202";
+
+
+    @Value("${app.proxy.url}")
+    private String proxyUrl = "http://localhost:8102";
+
+    @Value("${app.validator.url}")
+    private String validatorUrl = "http://localhost:8101";
+
+    @Value("${app.web.url}")
+    private String webUrl = "http://localhost:8080";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(billingUrl, proxyUrl, validatorUrl, webUrl);
+    }
+
+}
