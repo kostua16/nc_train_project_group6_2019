1a579d16853751581458cf7aa64dc87c055a6427
Konstantin Leontiev <konstantin.leontiev@netcracker.com>
2020-04-26 00:43:37 +0400

    add test ui for QA

---
+++ Billing/Billing-DB/src/main/java/com/edunetcracker/billingservice/BillingDB/TemplatesConfig.java
@@ -0,0 +1,7 @@
+package com.edunetcracker.billingservice.BillingDB;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TemplatesConfig {
+}
--- Billing/Billing-DB/src/main/java/com/edunetcracker/billingservice/BillingDB/WebConfig.java
+++ Billing/Billing-DB/src/main/java/com/edunetcracker/billingservice/BillingDB/WebConfig.java
@@ -1,10 +1,20 @@
 package com.edunetcracker.billingservice.BillingDB;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebMvc
@@ -27,5 +37,41 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins(billingUrl, proxyUrl, validatorUrl, webUrl);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/*").addResourceLocations(
+                "classpath:/static/",
+                "classpath:/APP-INF/resources/static/",
+                "classpath:/META-INF/resources/static/",
+                "classpath:/src/main/resources/static/",
+                "classpath:/META-INF/resources/webjars/"
+        );
+    }
+
+
+//    @Bean
+//    @ConfigurationProperties("spring.datasource")
+//    @Autowired
+//    @Primary
+//    public DataSource dataSource(DataSourceProperties properties) {
+//        String newUrl = properties.getUrl().replaceAll("postgres:", "postgresql:");
+//        if(!newUrl.startsWith("jdbc:")){
+//            newUrl = "jdbc:" + newUrl;
+//        }
+//        if(newUrl.startsWith("jdbc:postgresql://") && newUrl.contains("@")){
+//            String urlWithoutProtocol = newUrl.substring(18);
+//            String[] split = urlWithoutProtocol.split("@");
+//            String[] userPassPair = split[0].split(":");
+//            newUrl = "jdbc:postgresql://" + split[1];
+//            properties.setUsername(userPassPair[0]);
+//            properties.setPassword(userPassPair[1]);
+//            System.out.println(newUrl);
+//            System.out.println(userPassPair[0]);
+//            System.out.println(userPassPair[1]);
+//        }
+//        properties.setUrl(newUrl);
+//        return properties.initializeDataSourceBuilder().build();
+//    }
 
 }
